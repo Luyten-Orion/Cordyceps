7cc214f22556a02c0047752428f3d71c8f324aac
Luyten-Orion <raeeshussain960@gmail.com>
2024-06-03 21:26:46 +0100

    Improve macro and get it in a working state, create `unpack` and `dispatchIt` macro

--- src/cordyceps.nim
+++ src/cordyceps.nim
@@ -1,6 +1,14 @@
-import std/[strutils, macros]
+import std/[macros, strutils, typetraits, tables]
 
 import pkg/micros
+
+type
+  CordycepsUnpackDefect* = object of Defect
+
+# Couldn't figure out a good way to represent this using
+# a cache table
+var Stipes {.compileTime.}: Table[string, Table[string,
+  tuple[value, field: NimNode]]]
 
 proc `[]`(stmtList: StmtList, index: int): NimNode = stmtList.NimNode[index]
 proc `[]=`(stmtList: StmtList, index: int, value: StmtSubTypes) = stmtList.NimNode[index] = value.NimNode
@@ -16,7 +24,7 @@ proc stripEmptyPragmaExpr(typeDef: TypeDefs): TypeDefs =
       # If no unused pragmas are left:
       if res[0][1].len == 0:
         # Remove the empty pragma section
-        res[0][0][0] = res[0][0].copyNimTree()
+        res[0] = res[0][0].copyNimTree()
 
     else:
       discard
@@ -121,7 +129,7 @@ macro spore*(body): ObjectDef =
   if not hasOneField:
     error("`spore` requires at least one field to be defined!", body)
 
-  result = body
+  result = body.copyNimTree
 
   # Create a statement list and add an object spore (the internal implementation type)
   var typeSec = newNimNode(nnkTypeSection)
@@ -131,10 +139,6 @@ macro spore*(body): ObjectDef =
   var objSpore = sporeBody[0][0].objectDef
   if objSpore.NimNode[^1].kind == nnkRefTy:
     objSpore.NimNode[^1] = objSpore.NimNode[^1][0]
-
-  # Set the name and unmark it as exported if required
-  objSpore.exported = false
-  objSpore.name = $objSpore.name & "Spore"
 
   # Create distinct types for the spore and also make sure no conditional fields are defined yet,
   # since that's our job
@@ -146,48 +150,46 @@ macro spore*(body): ObjectDef =
 
     typeSec.insert 0, newNimNode(nnkTypeDef)
     var tp = typeSec[0]
-    tp.add field.name.NimNode
+    tp.add if not body.objectDef.exported:
+        field.name.NimNode
+      else:
+        exported($field.name).NimNode
     tp.add objSpore.genericParamList
     tp.add newNimNode(nnkDistinctTy)
     tp[2].add body.objectDef.accessObj
 
-
   # Create the enum's name and define the variables needed
   var
-    enumName = nimName(($objSpore.name)[0..<(^5)] & "Kind")
+    enumName = nimName($objSpore.name & "Kind")
     enumFields = newSeq[NimNode]()
     enumPrefix = ""
 
-  for c in $($objSpore.name)[0..<(^5)]:
+  for c in $objSpore.name:
     if c.isUpperAscii:
       enumPrefix.add c.toLowerAscii
 
   enumName.NimNode.copyLineInfo(objSpore.name.NimNode)
 
-  # Iterate over the fields in the spore and define the fields
-  for i in objSpore.fields:
-    var field = enumField($enumPrefix & $i.name, none(int))
+  # Create a table used for lookup
+  Stipes[$objSpore.name] = initTable[string, (NimNode, NimNode)]()
+
+  # Create template for code reuse
+  template eField(nm: NimName) =
+    var field = enumField($enumPrefix & $nm, none(int))
     #field.NimNode.copyLineInfo(i.NimNode)
     enumFields.add field.NimNode
 
-    #[
-    var initProc = routineNode(nimName("init"), rtProc)
-    initProc.addGeneric identDef(nimName("T"), i.name.NimNode)
-    initProc.addParam identDef(nimName("_"), nimName("T").NimNode)
-    initProc.returnType = nimName("T").NimNode
+    Stipes[$objSpore.name][$nm] = (
+      ident($field.name), ident($nm & "Data")
+    )
 
-    var initCall = newNimNode(nnkCall)
-    initCall.add i.name.NimNode
-    initCall.add newNimNode(nnkObjConstr)
-    initCall[^1].add body.objectDef.accessObj
-    initCall[^1].add newNimNode(nnkExprColonExpr)
-    initCall[^1][^1].add ident("kind")
-    initCall[^1][^1].add field.name.NimNode
-
-    # Add to bodies
-    initProc.addToBody initCall
-    sporeBody.add initProc
-    ]#
+  # Iterate over the fields in the spore and define the fields
+  for i in objSpore.fields:
+    if i.isSingle:
+      eField(i.name)
+    else:
+      for nm in i.names:
+        eField(nm)
 
   # Create the enum and add it to the list
   var enumSpore = enumDef(enumName, enumFields, body.objectDef.exported)
@@ -200,38 +202,130 @@ macro spore*(body): ObjectDef =
 
   recCase.add identDef(exported("kind"), enumName.NimNode)
 
-  for i in objSpore.fields:
+  template cStmt(i: IdentDef, nm: NimName) =
     recCase.add newNimNode(nnkOfBranch)
-    let enumCase = nimName(enumPrefix & $i.name)
+    let enumCase = nimName(enumPrefix & $nm)
     recCase[^1].add enumCase.NimNode
     recCase[^1].add newNimNode(nnkRecList)
-    var field = identDef(exported($i.name & "Data"), i.typ)
-    field.name.NimNode.copyLineInfo(i.name.NimNode)
+    var field = identDef(exported($nm & "Data"), i.typ)
+    field.name.NimNode.copyLineInfo(nm.NimNode)
     recCase[^1][^1].add field.NimNode
 
-  # Edit the spore body
+  for i in objSpore.fields:
+    if i.isSingle:
+      cStmt(i, i.name)
+    else:
+      for nm in i.names:
+        cStmt(i, nm)
+  # Clear the fields
   objSpore.recList = newNimNode(nnkRecList, objSpore.recList)
+
+  # Edit the spore body
   objSpore.recList.add recCase
 
-  # Add the return type to the statement list
-  if body[^1].kind == nnkRefTy:
-    sporeBody.add newNimNode(nnkRefTy)
-    sporeBody.NimNode[^1].add objSpore.accessObj
-  else:
-    sporeBody.add objSpore.accessObj
-
   # Set the body of the returned type to the statement list
-  result[^1] = sporeBody.NimNode
+  typeSec.add objSpore.NimNode
 
-  typeSec.add result
+  #typeSec.add result
   result = typeSec
 
-  echo result.repr
-  echo result.treeRepr
+proc resolveUnderlyingType(typ: NimNode): NimNode =
+  if typ.getImpl[^1].kind == nnkDistinctTy:
+    typ.getImpl[^1][^1]
+  else:
+    typ.getImpl[^1]
 
-type
-  MOption*[T] {.spore.} = ref object
-    None: void
-    Some: T
+macro unpack*(target: typed) =
+  var typ = target.getTypeInst
+  var lastTypSym: NimNode
+  template typSym: NimNode = (if typ.kind in {nnkSym, nnkEmpty}: typ else: typ[0])
+  var base: NimNode = typSym
 
-Some[int].init()
+  while true:
+    lastTypSym = typSym
+    if base.kind == nnkSym and base.strVal notin Stipes:
+      typ = resolveUnderlyingType(typSym)
+      while base.kind != nnkSym or base.strVal notin Stipes:
+        base = typSym.getImpl[^1][0]
+        if base.kind notin {nnkSym, nnkEmpty}:
+          lastTypSym = typSym
+          base = base[0]
+        else:
+          break
+        continue
+    break
+
+  if base.kind == nnkEmpty:
+    typ = target.getTypeInst
+    base = typSym.getImpl
+    base = (if base.kind in {nnkSym, nnkEmpty}: base else: base[^1][0])
+    base = (if base.kind in {nnkSym, nnkEmpty}: base else: base[0])
+
+  if base.strVal != lastTypSym.strVal:
+    if lastTypSym.strVal notin Stipes[base.strVal]:
+      error("`" & target.strVal & "` isn't a valid distinct sum type of `" & base.strVal & "`!", target)
+
+  else:
+    error("You must use a distinct sum type!", target)
+
+  var ifRes = ifStmt()
+
+  let elifCond = infix(
+    newDotExpr(newCall(typ.getTypeImpl[0], target),
+      ident("kind")),
+      "==", newDotExpr(ident(base.strVal & "Kind"),
+      Stipes[base.strVal][typSym.strVal].value
+    )
+  )
+  let elifBody = newDotExpr(newCall(typ.getTypeImpl[0], target),
+    Stipes[base.strVal][typSym.strVal].field)
+
+  var elifRes = elifBranch(elifCond, elifBody)
+
+  var elseRes = newNimNode(nnkElse)
+  elseRes.add quote do:
+    raise newException(typedesc[`CordycepsUnpackDefect`],
+      "Couldn't unpack the given object because of the discrepancy between the type and the internal object kind!")
+
+  ifRes.add elifRes
+  ifRes.add elseRes.elseBranch
+  result = ifRes.NimNode
+
+macro dispatchIt*(target: typed, body: untyped): untyped =
+  var typ = target.getTypeInst
+  template typSym: NimNode = (if typ.kind == nnkSym: typ else: typ[0])
+
+  while typ.kind != nnkEmpty and typSym.strVal notin Stipes:
+    typ = resolveUnderlyingType(typSym)
+
+  if typSym.strVal notin Stipes:
+    error("Can't unpack the target as it only operates on unknowns!", target)
+
+  result = newNimNode(nnkStmtList)
+
+  let tmp = genSym(nskLet, ":tmp")
+
+  result.add quote do:
+    let `tmp` = `target`
+
+  let res = caseStmt(nimName("_"))
+  res.discriminator[] = newDotExpr(tmp, ident("kind"))[]
+
+  for field in Stipes[typSym.strVal].keys:
+    let tup = Stipes[typSym.strVal][field]
+    var bod = typ.copyNimTree()
+    if typ.kind != nnkSym:
+      bod[0] = ident(field)
+    else:
+      bod = ident(field)
+
+    var stmt = stmtList(newNimNode(nnkStmtList))
+    #stmt.add newLetStmt(ident("it"), newCall(bod, target))
+    let it = ident("it")
+    stmt.add quote do:
+      template `it`: auto {.used.} = `bod`(`tmp`)
+    stmt.add body.copyNimTree()
+
+    res.add ofBranch(tup.value, stmt.NimNode)
+
+  result.add res.NimNode
